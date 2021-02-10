979ea08c8dc74fca2803634bf198b0ee40c6330f
Thomas Virgo <tom.virgo@fairheat.com>
2021-02-10 17:44:42 +0000

    added listing task feature using state hook

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ const App = () => {
   const [tasks, setTasks] = useState([]);
   const [userInput, setUserInput] = useState('');
 
-  console.log(tasks);
-
   const handleChange = (event) => {
     setUserInput(event.target.value)
   }
@@ -19,14 +17,20 @@ const App = () => {
     setTasks([...tasks, userInput]); //cannot use push, have to create new tasks, hence use spread ...
   }
 
+  const clearInput = (event) => {
+    event.preventDefault();
+    setUserInput('');
+  }
+
 
   return (
     <div>
       <h1>Welcome!</h1>
       <button onClick = {() => setCounter(count + 1)} >Click Me pleasio!</button>
       <p>Current count is: {count}</p>
-      <Form userInput={userInput} handleChange={handleChange} handleSubmit={handleSubmit}/>
+      <Form userInput={userInput} handleChange={handleChange} handleSubmit={handleSubmit} clearInput={clearInput}/>
       <p>User input is: {userInput}</p>
+      <List tasks={tasks}/>
     </div>
   )
 }
@@ -37,7 +41,17 @@ const Form = (props) => {
     <form onSubmit = {props.handleSubmit}>
       <input type='text' value = {props.userInput} onChange = {props.handleChange}></input>
       <button type = 'submit'>Add Task</button>
+      <button onClick = {props.clearInput}>Clear Input</button>
     </form>
+  )
+}
+
+const List = (props) => {
+  const taskElements = props.tasks.map((item, index) => <h2 key = {index}>{item}</h2>)
+  return (
+    <div>
+      {taskElements}
+    </div>
   )
 }
 
