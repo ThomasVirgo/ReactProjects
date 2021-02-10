bd7de9822eda928eb4c8a29c19f0c1a86dd31aa1
Thomas Virgo <tom.virgo@fairheat.com>
2021-02-10 17:26:36 +0000

    removed class based components and moved to using state hook

--- src/App.js
+++ src/App.js
@@ -1,81 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-//importing components
 
 
+const App = () => {
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      userInput: '',
-      toDoList: [],
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.deleteTask = this.deleteTask.bind(this);
+  const [count, setCounter] = useState(5);
+  const [tasks, setTasks] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  console.log(tasks);
+
+  const handleChange = (event) => {
+    setUserInput(event.target.value)
   }
 
-  handleChange(event){
-    this.setState({
-      userInput:event.target.value,
-    });
+  const handleSubmit = (event) => {
+    event.preventDefault(); //prevents the page from reloading when the submit button is clicked
+    setTasks([...tasks, userInput]); //cannot use push, have to create new tasks, hence use spread ...
   }
 
-  handleSubmit(event){
-    event.preventDefault();
-    this.setState(state=>({
-      toDoList: [...state.toDoList, state.userInput], //cant use push, treat as if immutable!
-    }))
-  }
 
-  deleteTask(task){
-    let myTasks = [...this.state.toDoList];
-    let indexOfDelete = myTasks.indexOf(task);
-    myTasks.splice(indexOfDelete,1);
-    this.setState({
-      toDoList: myTasks,
-    })
-  }
-
-  render(){
-    return (
-      <div>
-        <header>
-          <h1>To Do List</h1>
-        </header>
-        <Form input = {this.state.userInput} handleChange = {this.handleChange} handleSubmit = {this.handleSubmit} />
-        <List tasks = {this.state.toDoList} deleteTask = {this.deleteTask} />
-      </div>
-    )
-  }
-}
-
-const Form = (props) => {
   return (
     <div>
-      <form onSubmit = {props.handleSubmit}>
-        <input type='text' value={props.input} onChange = {props.handleChange} />
-        <button type = 'submit'>Add Task</button>
-      </form>
+      <h1>Welcome!</h1>
+      <button onClick = {() => setCounter(count + 1)} >Click Me pleasio!</button>
+      <p>Current count is: {count}</p>
+      <Form userInput={userInput} handleChange={handleChange} handleSubmit={handleSubmit}/>
+      <p>User input is: {userInput}</p>
     </div>
   )
 }
 
 
-const List = (props) => {
-  let tasks = props.tasks;
-  let tasksHTML = tasks.map(task => 
-    <div className = 'task-item'>
-      <h3 key={task}> {task} </h3>
-      <button onClick = {() => {props.deleteTask(task)}}>Delete Task</button>
-    </div>
-   );
+const Form = (props) => {
   return (
-    <div>
-      {tasksHTML}
-    </div>
+    <form onSubmit = {props.handleSubmit}>
+      <input type='text' value = {props.userInput} onChange = {props.handleChange}></input>
+      <button type = 'submit'>Add Task</button>
+    </form>
   )
 }
 
 export default App;
+
