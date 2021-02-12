dab25bcbbf1567c84efce7f1d44b95e8171c739e
Thomas Virgo <tom.virgo@fairheat.com>
2021-02-12 14:36:35 +0000

    added filter for natural events

--- src/components/event-tracker/EventTrackerMain.js
+++ src/components/event-tracker/EventTrackerMain.js
@@ -3,20 +3,31 @@ import NaturalEvents from './NaturalEvents'
 
 const NaturalEventTracker = (props) => {
     const [events, setEvents] = useState([]);
+    const [dropdown, setDropdown] = useState(false);
 
     useEffect(()=>{
         const fetchEventData = async () => {
-        const myResponse = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit=10&days=10'); //have to use await because fetch returns a promise
+        const myResponse = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit=50&days=100'); //have to use await because fetch returns a promise
         const data = await myResponse.json();
         setEvents(data.events);
-        console.log(data.events);
         }
         fetchEventData()
     }, []) //providing an empty array as the dependency means it only executes once when the page loads. If you provide nothing, it will execute on every re-render!
 
+    const toggleDropdown = () => {
+        setDropdown(!dropdown);
+    }
+
+    const filterStyle = {
+        backgroundColor: 'Blue',
+        color: 'White',
+        fontSize: 20,
+    };
+
     return (
         <div id='natural-event-tracker'>
-            <NaturalEvents events = {events} />
+            <button id='dropdown' onClick = {toggleDropdown} style = {filterStyle}>Filter</button>
+            <NaturalEvents events = {events} isDropdown = {dropdown} />
         </div>
     )
 }
--- src/components/event-tracker/NaturalEvents.js
+++ src/components/event-tracker/NaturalEvents.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const NaturalEvents = ({events}) => {
-    let filteredEvents = events.map(function(item){
-      let myObj = {
-        'title':item.title,
-        'type':item.categories[0].title,
-        'location':item.geometries,
-      }
-      return myObj
-    })
-    let mappedEvents = filteredEvents.map(item => <p key = {item.title}>{`${item.title} is a ${item.type}`}</p>)
+const NaturalEvents = ({events, isDropdown}) => {
+
+  const [showEvents, setShowEvents] = useState([]);
+
+  let myEvents = events.map(function(item){
+    let myObj = {
+      'title':item.title,
+      'type':item.categories[0].title,
+      'location':item.geometries,
+    }
+    return myObj
+  })
   
+  const mappedEvents = myEvents.map(item => <p key = {item.title}>{item.title}</p>)
+  const reduceFun = (list,item) => {
+    if (list.indexOf(item.type)==-1){
+      list.push(item.type);
+    }
+    return list
+  }
+  let eventTypes = myEvents.reduce(reduceFun,[]);
+  
+  const filterEvents = (myEvents, eventType) => {
+    return myEvents.filter(item => item.type == eventType);
+  };
+
+  const handleFilterClick = (eventType) => {
+    const filteredEvents = filterEvents(myEvents, eventType);
+    const showTheseEvents = filteredEvents.map(item => <p key = {item.title}>{item.title}</p>);
+    setShowEvents(showTheseEvents);
+  }
+
+  const filterButtons = eventTypes.map(item => {
+    const myButton = <button key={item} onClick = {()=>handleFilterClick(item)}>{item}</button>
+    return myButton;
+  })
+
+  if (isDropdown){
     return (
       <div>
-        <h5>{mappedEvents}</h5>
+        <div id = 'filter-buttons'>{filterButtons}</div>
+        <div id = 'events1'>{showEvents}</div>
       </div>
     )
-  }
+  } else {
+    return (
+    <div>
+      <div id = 'events2'>{mappedEvents}</div>
+    </div>
+  )} 
+}
 
 export default NaturalEvents;
