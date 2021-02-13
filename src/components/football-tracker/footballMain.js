2538deb0b3aa33333c7ea3cbfd9859ecdb7645af
ThomasVirgo <74493318+ThomasVirgo@users.noreply.github.com>
2021-02-13 11:30:11 +0000

    Add files via upload
    Added football api

--- src/components/football-tracker/footballMain.js
+++ src/components/football-tracker/footballMain.js
@@ -2,6 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 
 const FootballMain = (props) => {
+
+    const [results, setResults] = useState([]);
+
+    useEffect(()=>{
+        const fetchResultsData = async () => {
+        const myResponse = await fetch('https://api.football-data.org/v2/matches', {
+            method: 'GET',
+            headers: {
+                'X-Auth-Token': 'e6c9b3fd6cfe4b81a41bf981997c594f' 
+            }
+        }); 
+        const data = await myResponse.json();
+        setResults(data);
+        }
+        fetchResultsData()
+    }, []) 
+
+    console.log(results);
+    
     return (
         <div>
             <h1>Football!</h1>
